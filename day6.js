0e33932512cfe5ead02c09e46b77799680a3fefc
Darshana-Bhandari <bandaridarshana@gmail.com>
2026-06-26 19:38:08 +0545

    Update project

--- day6.js
+++ day6.js
@@ -7,9 +7,9 @@ console.log(age);
 const pi = 3.14;
 
 // Data Types
-let number = 23; // number
-const name = "Darshana2063"; // string
-let isLoggedIn = true; // boolean
+let number = 23;
+const name = "Darshana2063";
+let isLoggedIn = true;
 let city;
 let user = null;
 
@@ -19,56 +19,35 @@ console.log(typeof city);
 // Arithmetic Operations
 let x = 4;
 let y = 6;
-let sum = x + y;
 
-console.log(sum); //addition 
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
 
-let r=14;
-let b=10;
-console.log(r-b); //subtraction
-console.log(r*b); //multiplication
-console.log(r/b) //division
-console.log(r%b);
+// Comparison Operators
+console.log(age == 22);
+console.log(age != 22);
+console.log(age >= 45);
+console.log(age <= 10);
 
-// comparision operators
-// == equale to
-// != not equal to
-// > greater than
-// < less than
-// >= greater than equal to
-// <= less then equal to
-// == Strict equal to 
+// Logical Operators
+let ag = 22;
+let citizen = true;
 
-let age =22;
-console.log(age==22); //checking equal to
-console.log(age!=22); //checking not  equal to
-console.log(age>=45); //checking greater than equal to
-console.log(age<=10); //checking less than equal to
+console.log(ag >= 18 && citizen == true);
 
-// logic operators
-//&&
-let ag=22;
-let citizen=true;
-console.log(ag>=18 && citizen==true);
+citizen = false;
+console.log(ag >= 18 && citizen == true);
 
-let a=22;
-let citizen=false;
-console.log(a>=18 && citizen==true);
+console.log(ag >= 18 || citizen == true);
 
-let B=22;
-let citizen=false;
-console.log(B>=18 || citizen==true);
-
-//assigment operators
-let D=5;
-E+=20;
+// Assignment Operators is all about increment and decrement
+let D = 5;
+D += 20;
 console.log(D);
 
-//assigment operators
-let E=5;
-E+=20;
+let E = 5;
+E += 20;
 console.log(E);
-
-
-
-
