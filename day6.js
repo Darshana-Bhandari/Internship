542a573fe0fcd98fc3996cf5cfcd794b7b00c81b
Darshana-Bhandari <bandaridarshana@gmail.com>
2026-06-26 19:04:46 +0545

    Update project

---
+++ day6.js
@@ -0,0 +1,24 @@
+// Variables
+let age = 22;
+age = 23;
+age = 24;
+console.log(age);
+
+const pi = 3.14;
+
+// Data Types
+let number = 23; // number
+const name = "Darshana2063"; // string
+let isLoggedIn = true; // boolean
+let city;
+let user = null;
+
+console.log(typeof name);
+console.log(typeof city);
+
+// Arithmetic Operations
+let x = 4;
+let y = 6;
+let sum = x + y;
+
+console.log(sum);
