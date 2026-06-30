f106f67dd788fa6253df23f0f6ac986cdd6666bd
Darshana-Bhandari <bandaridarshana@gmail.com>
2026-06-30 18:32:06 +0545

    Describe your changes

--- Day8.js
+++ Day8.js
@@ -4,13 +4,25 @@
 
 while(Condition){
     //
-console.log("execute garxa run garxa code lei"):
+console.log("execute garxa run garxa code lei");
 }
 
 // example of the while loop 
 
-let count=1;
+let count=11;
 while(count<=10){
     console.log("Number are"+count);
     count ++
 }
+
+
+// do while loop
+
+// first time yelei code lei exceute garxa anu matara yele condition check garxa
+let count= 1
+do{
+    console.log("the number are" + count)
+}while(count>=10)
+
+
+
