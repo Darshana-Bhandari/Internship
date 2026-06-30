aea15baa7ed62f605fd5e09829aebecbec4f1781
Darshana-Bhandari <bandaridarshana@gmail.com>
2026-06-30 19:30:38 +0545

    Describe your changes

---
+++ Day8_task.js
@@ -0,0 +1,35 @@
+// OBJEct
+// Vegetable
+
+let vegetable = {
+    name: "Carrot",
+    color: "Orange",
+    season: "all season"
+};
+
+console.log(vegetable);
+
+console.log(vegetable.name);
+console.log(vegetable.color);
+console.log(vegetable.Season);
+
+// OBJECT
+// Fruits
+
+let fruit = {
+    name: "Mango",
+    color: "Yellow",
+    taste: "Sweet"
+};
+
+console.log(fruit);
+
+console.log(fruit.name);
+console.log(fruit.color);
+console.log(fruit.taste);
+
+fruit.color = "Green";
+
+fruit.price = 120;
+
+console.log(fruit);
