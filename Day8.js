ff608ff2332609c50fc3776661113fd5210f0dfb
Darshana-Bhandari <bandaridarshana@gmail.com>
2026-06-30 18:24:31 +0545

    Add latest internship updates

---
+++ Day8.js
@@ -0,0 +1,16 @@
+// while loop
+
+// while loop execute garnu vanda pahila nei yele codition check garxa
+
+while(Condition){
+    //
+console.log("execute garxa run garxa code lei"):
+}
+
+// example of the while loop 
+
+let count=1;
+while(count<=10){
+    console.log("Number are"+count);
+    count ++
+}
