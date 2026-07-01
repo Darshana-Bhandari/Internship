128b974895dcea8d89d6b2e09d901133a60730a2
Darshana-Bhandari <bandaridarshana@gmail.com>
2026-07-01 18:47:43 +0545

    Add employee object assignment practice

---
+++ day9.js
@@ -0,0 +1,17 @@
+let fruits=["Apple","mango","Graps"]
+//includes
+console.log(fruits.includes("basketball")) /// it helps us to sheck the element exists or not
+
+//indexof
+console.log(fruits.indexoof("Apple")) ///it gives us the position of an element in an array
+
+//short
+fruirs.sort()
+console.log(fruits)
+
+//reverse
+fruits.reverse()
+console.log(fruits)
+
+///splice method
+
