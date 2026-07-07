c35bce6e6f7c0f63424d09df1ba3e011590f2ef6
Darshana-Bhandari <bandaridarshana@gmail.com>
2026-07-07 22:33:19 +0545

    Refactor Fetch API examples and improve code readability

--- day12.js
+++ day12.js
@@ -1,26 +1,109 @@
-// Fetch API is a built-in JavaScript feature used to send HTTP requests
-// to a server and receive data from it.
+ //is commonly used to:
+// -get data (GET)
+// - send data (POST)
+// --update data (PUT/PATCH)
+// -- delete data (DELETE)
 
-// Common HTTP methods:
-// GET    - Retrieve data
-// POST   - Create new data
-// PUT/PATCH - Update existing data
-// DELETE - Remove data
 
-// Why do we use the Fetch API?
-// Imagine we are building an e-commerce website.
-// The frontend needs to communicate with the backend
-// to fetch products, user information, orders, etc.
-// Without the Fetch API, the website cannot exchange
-// data with the server.
 
-// Example: GET Request
+// why do we use fetch api?
+//- imagine we are building:
+// E-commerce webiste
+// the frontend needs data fron the backend
+// without fetch api , our website cannot communicate with the server
 
-fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then((response) => response.json())
-  .then((data) => {
-    console.log('Fetched Data:', data);
-  })
-  .catch((error) => {
-    console.error('Error fetching data:', error);
-  });
+// let url=http://localhost/5000/api
+//basic get request (GET)
+// fetch("https://jsonplaceholder.typicode.com/users") //  sending the request to the server
+// .then (function(response){ 
+    
+//      /// receives the server response 
+//      console.log("loading the data ....")
+
+//     return response.json();  // converts the json into a javascript object
+// })
+
+
+// .then(function(data){ /// displays the received data
+   
+//         // console.log(userdata.username)
+
+//         let output=""
+//         data.forEach(function(user){
+//             output+=`<li>${user.name}</li>`;
+//             output+=`<li>${user.email}</li>`;
+            
+//         });
+//         document.getElementById("users").innerHTML=output;
+//     })
+
+    // /// fetch using the aynsc and await
+    //  async  function getUsers(){
+    //     let response = await fetch("https://jsonplaceholder.typicode.com/users") /// receives the server response 
+    //     let users = await response.json()
+    //     console.log(users)
+    //   }
+
+    //   getUsers()
+
+
+
+
+
+
+//POST request (POST) // users send data 
+//fetch(url,optionsvalue)
+
+fetch("https://jsonplaceholder.typicode.com/posts",{
+    method:"POST",
+    headers:{    /// headers provide the extra information about the request
+        "Content-Type":"application/json"   // ma json format ma data lei send gardei xu
+       
+    },
+    body:JSON.stringify({  // converts the javascript objects into a json string
+        title:"javascript learning or fetch post requests learnig",
+        body:"learning fetch api",
+        userdId:1
+    })
+})
+
+
+.then(function(response){
+     return response.json();
+
+})
+.then(function(data){
+    console.log(data)
+})
+
+
+//// only update the title (patch/PUT)
+fetch("https://jsonplaceholder.typicode.com/posts/1",{
+    method:"PUT",
+    headers:{
+        "Content-Type":"application/json"
+    },
+    body:JSON.stringify({
+        title:"new title",
+        body:"new data updated"
+    })
+})
+
+.then(function(response1){
+     return response1.json();
+
+})
+.then(function(data1){
+    console.log(data1)
+})
+
+// delete the data(DELETE)
+fetch("https://jsonplaceholder.typicode.com/posts/1",{
+    method:"DELETE",
+
+})
+
+.then(function(response2){
+    console.log("post deleted")
+
+})
